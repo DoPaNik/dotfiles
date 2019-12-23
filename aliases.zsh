905a84b258eedd8ed697892eb54557e7806f6fc6
DoPaNik <963151+DoPaNik@users.noreply.github.com>
2019-12-23 16:49:44 +0100

    Common Configuration for Zsh :white_check_mark:

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+alias reload="exec ${SHELL} -l"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias c="clear"
+
+alias nt="cd $HOME/Documents/Novatec"
+alias bit="cd $HOME/Documents/BrainActivIT"
+alias dopamin="cd $HOME/Documents/DOPAmin"
+alias zshconfig="code $HOME/.zshrc"
+
+weather() { curl -4 wttr.in/${1:-stuttgart} }
---
+++ path.zsh
@@ -0,0 +1,17 @@
+# Add GNU core utilities to beginning
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+#Kubernetes Helper Configurations
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export KUBE_PS1_SYMBOL_USE_IMG=true
+source "$(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh"
+
+#Magic Navigation Z Configurations
+. $(brew --prefix)/etc/profile.d/z.sh
+
+# Source iTerm2 Shell Integration
+# https://iterm2.com/documentation-shell-integration.html
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+eval $(thefuck --alias)
+eval "$(pyenv init -)"
