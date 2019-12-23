10050fc912da99a9b3548fedcf8a177682ca4d2c
DoPaNik <963151+DoPaNik@users.noreply.github.com>
2019-12-23 17:10:56 +0100

    :rocket: Add Installtion Script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Install "Oh My Zsh"
+if test ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh"; then
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Link Dotfiles to my $HOME
+ln -s $PWD $HOME/.dotfiles
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
