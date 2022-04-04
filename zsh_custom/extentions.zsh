e763a35eb54a5ce82344578158eb544161358424
DoPanik <963151+DoPaNik@users.noreply.github.com>
2022-04-04 21:56:21 +0200

    :lipstick: Configure extensions

--- zsh_custom/extentions.zsh
+++ zsh_custom/extentions.zsh
@@ -9,8 +9,20 @@ export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 #export KUBE_PS1_SYMBOL_USE_IMG=true
 POWERLEVEL9K_MODE='awesome-fontconfig'
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(kubecontext context dir vcs)
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(kubecontext context dir vcs newline)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator command_execution_time time)
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+
+
+#Colorize Man pages
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
 
 #Magic Navigation Z Configurations
 . $(brew --prefix)/etc/profile.d/z.sh
